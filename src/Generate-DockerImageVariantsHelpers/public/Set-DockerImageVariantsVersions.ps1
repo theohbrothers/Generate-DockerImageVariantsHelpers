e0b2b3714befdb1fd1333ade2ab18d8ce802cd27
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-04 01:28:19 +0000

    Enhancement: Add setting empty array in `versions.json` in `Set-DockerImageVariantsVersions`

--- src/Generate-DockerImageVariantsHelpers/public/Set-DockerImageVariantsVersions.ps1
+++ src/Generate-DockerImageVariantsHelpers/public/Set-DockerImageVariantsVersions.ps1
@@ -5,5 +5,8 @@ function Set-DockerImageVariantsVersions {
     )
     $VERSIONS_JSON_FILE = "./generate/definitions/versions.json"
     "Writing $VERSIONS_JSON_FILE" | Write-Host -ForegroundColor Green
-    $versions | Sort-Object -Descending | ConvertTo-Json -Depth 100 | Set-Content $VERSIONS_JSON_FILE -Encoding utf8
+    if ($Versions -is [array]) {
+        $Versions = ,$Versions
+    }
+    $Versions | ConvertTo-Json -Depth 100 | Set-Content $VERSIONS_JSON_FILE -Encoding utf8
 }
